56a1917803f7d24ba2fc4162831626387ada822c
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-03-27 00:23:45 +0400

    bug-fix:returning null problem because of executing dom element executes before DOM fully loads,feature:btnEl was tested

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+window.onload=function(){
+ const App = document.getElementById("app");
+const btnEl = App.querySelector(".addNote");
+btnEl.addEventListener("click",()=>{console.log("clicked");})
+}
